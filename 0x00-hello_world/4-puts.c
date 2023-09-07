7e13145fe2f149db70bc47efccd137f420ef9be7
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-09-07 15:32:56 -0700

    using the puts function to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main entry point
  *
- * progthat print using puts function
+ *Descreiption:A c program that print using puts function
  *
  * Return: Always 0 (success)
  */
 
 int main(void)
-{	
+{
 	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
 	return (0);
