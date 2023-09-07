cdb039ab1f73355118b257ee4e088ef6d8a6603a
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-09-07 16:54:57 -0700

    gitting the size of various types on the computer it's compiled on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,15 +20,11 @@ int main (void)
 
 	float f;
 
-	printf("size of a char: %lu /n", (unsigned long)sizeof(c));
-
-	printf("size of an int: %lu /n", (unsigned long)sizeof(i));
-
-	printf("size of a long int: %lu /n", (unsigned long)sizeof(l));
-	
-	printf("size of a long long int: %lu /n", (unsigned long)sizeof(ll));
-
-	printf("size of a float: %lu /n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu \n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu \n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu \n", (unsigned long)sizeof(l));
+	printf("Size of a long long int: %lu \n", (unsigned long)sizeof(ll));
+	printf("Size of a float: %lu \n", (unsigned long)sizeof(f));
 
 	return (0);
 }
