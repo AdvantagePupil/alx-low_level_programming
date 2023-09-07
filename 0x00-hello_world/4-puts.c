f78264f0ef2da2cbe4975c9d785c90a680cc1c1d
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-09-07 13:35:21 -0700

    using the function puts instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/*the puts command is similar to the command printf but the difference is that the command puts prints only stringss */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	char str[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str);
 	return (0);
 }
