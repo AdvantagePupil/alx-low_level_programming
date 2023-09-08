4717c9cb5003fed5ae66bb08d38dffd98b32e5da
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-09-07 17:39:22 -0700

    printing a string without using printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  main - entry point
+ *
+ *  Description: A C program that prints a string without using printf or puts
+ *
+ * Return: always 1 (success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
