8aaa6699f79793485f049c63e7aeebdf157fff33
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-09-12 16:34:38 -0700

    checking lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,9 @@
 /**
  *  _islower : print 1 if the charachter is lowercase else print 0
  *
- *  Description:  a function that checks for lowercase character.
+ *  Description:  a function that checks for lowercase character and prints 1 if they are.
+ *
+ *  c: the char is in ASCII code
  *
  *  Return: 1 if lower case and 0 if else
  */
