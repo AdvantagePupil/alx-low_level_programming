25bcd2dac784f2f4c3833c8fea3e5abeedba3b37
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-09-12 15:59:48 -0700

    printing the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Description: print the alphabet using _putchar
+ *
+ * Return: always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for(i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
