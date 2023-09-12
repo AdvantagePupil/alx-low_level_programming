3803abdda2669a5a7f6ca09e994cf9240b9281a5
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-09-12 15:50:40 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,13 @@
 #include "main.h"
-/*
+
+/**
  * main - Entry point
  *
  * Description: a program that prints _putchar
  *
  * Return: always 0 (success)
  */
+
 int main(void)
 {
 	_putchar('_');
