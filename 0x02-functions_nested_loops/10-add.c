db041490a74b638cefda1224d8e8189f2bcf0ae1
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-09-12 18:20:18 -0700

    getting the sum of 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * add description: a function that adds two integers and returns the result.
+ *
+ * @a: the input number is integer
+ *
+ * @b: the input number is integer
+ *
+ * Return: sum value
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
