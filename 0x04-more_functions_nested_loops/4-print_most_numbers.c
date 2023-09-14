b3e643020971f7244a12faf7430a017aaf57bbd2
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-09-14 13:42:00 -0700

    numbers from 0 to 9 but we hate 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * print_most_numbers description: print num from 0 to 9 but not 2 and 4
+ *
+ * Return: always 0 (success)
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+
+		_putchar(i);
+		
+		}
+	}
+	_putchar('\n');
+}
