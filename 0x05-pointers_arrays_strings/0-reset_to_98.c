71a157258de3d47458dc52422b8e01df087ed5a0
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-09-18 16:22:47 -0700

    changed the value of the pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ *
+ * description: a function that updates the value of a pointer to 98
+ *
+ * @n: the pointer
+ *
+ * REturn: the new value of the pointer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
