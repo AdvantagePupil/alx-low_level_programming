93ec16e1fd1d5cf56b26cec8dbec36a93706b30d
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-10-04 17:54:45 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,7 @@ int **alloc_grid(int width, int height)
 
 	int **n;
 
-	n = malloc(sizeof(n) * height);
+	n = malloc(sizeof(*n) * height);
 
 	if (width <= 0 || height <= 0 || n == 0)
 	{
@@ -30,7 +30,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-		n[i] = malloc(sizeof(n) * width);
+		n[i] = malloc(sizeof(**n) * width);
 
 		if (n[i] == NULL)
 		{
