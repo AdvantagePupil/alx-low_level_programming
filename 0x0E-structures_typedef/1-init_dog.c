7f1ad603207de9282f0cddeb1bce4e0bb7dbe209
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-10-10 15:47:28 -0700

    happy dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+
+/**
+ * init_dog - Entry point
+ *
+ * description: a function that initialize a variable of type struct dog
+ *
+ * @d: struct dog
+ *
+ * @name: dog name
+ *
+ * @age: dog age
+ *
+ * @owner: owner name
+ *
+ * Return: always 0 (success)
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
