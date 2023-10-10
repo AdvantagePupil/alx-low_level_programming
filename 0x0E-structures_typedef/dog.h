f994e69392f89ae8e0ef4dab52861e03cf26ba21
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-10-10 14:57:12 -0700

    happy dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Entry point
+ *
+ * @name: name of the dog
+ *
+ * @age: age of the dog
+ *
+ * @owner: dog owner
+ *
+ * Return: 0 always (succuss)
+ */
+
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+};
+#endif
