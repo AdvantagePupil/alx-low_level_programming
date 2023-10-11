bd09e0679d4b5ff52d6a2d5c9d25558a1f5d5512
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-10-11 15:29:01 -0700

    nice

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,9 +16,7 @@
 
 int main(int argc, char *argv[])
 {
-	char *op;
-
-	int n1, n2;
+	int (*op_func)(int,int), n1, n2;
 
 	if (argc != 4)
 	{
@@ -27,21 +25,21 @@ int main(int argc, char *argv[])
 	}
 
 	n1 = atoi(argv[1]);
-	op = argv[2];
+	op_func = get_op_func(argv[2]);
 	n2 = atoi(argv[3]);
 
-	if (get_op_func(op) == NULL || op[1] != '\0')
+	if (!op_func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*op == '/' && n2 == 0) || (*op == '%' && n2 == 0))
+	if (!n2 && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(op)(n1, n2));
+	printf("%d\n", op_func(n1, n2));
 
 	return (0);
 }
