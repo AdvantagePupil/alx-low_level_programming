6374a86bd7e2086ef15df03c6063ef0224c1c191
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-10-19 15:28:17 -0700

    linked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * list_len - Entry point
+ *
+ * description: a function that returns the number of elements in a linked list_t list
+ *
+ * @h: pointer
+ *
+ * Return: the number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t node;
+
+	node = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		node++;
+	}
+	return (node);
+}
