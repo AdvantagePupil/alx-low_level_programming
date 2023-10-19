c9734a2ccf66c309ff5c2a1e3f3279f3bc5dc40a
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-10-19 15:17:20 -0700

    first linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_list - Entry point
+ *
+ * description: a function that prints all the elements of a list_t list
+ *
+ * @h: pointer
+ *
+ * Return: elements of list-t list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node = 0;
+
+	const list_t *current = h;
+
+	for (; current != NULL; current = current->next)
+	{
+		if (current->str == NULL)
+		{
+			printf("[%d] %s\n", 0, "(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", current->len, current->str);
+		}
+		node++;
+	}
+	return (node);
+}
