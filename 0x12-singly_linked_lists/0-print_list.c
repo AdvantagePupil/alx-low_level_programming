609bd811494571cf6d88a3586f242f2d9d3bf1aa
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-10-19 17:19:20 -0700

    tried for loop but the checker still gave me one error so trying while loop

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,17 +14,18 @@ size_t print_list(const list_t *h)
 {
 	size_t node = 0;
 
-	while (h != NULL)
+	const list_t *current = h;
+
+	for (; current != NULL; current = current->next)
 	{
-		if (h->str == NULL)
+		if (current->str == NULL)
 		{
 			printf("[%d] %s\n", 0, "(nil)");
 		}
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", current->len, current->str);
 		}
-		h = h->next;
 
 		node++;
 	}
