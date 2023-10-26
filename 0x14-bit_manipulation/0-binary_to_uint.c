a7aed1e4215d082af2d49e65a775636d037ed1bf
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-10-26 14:45:19 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,21 +15,17 @@ unsigned int binary_to_uint(const char *b)
 	int n = 0;
 	unsigned int j = 0;
 
-	if (!b)
-	{
-		return (0);
-	}
-	if (b[n] != '\0' && b[n] != '1')
+	if (b == NULL)
 	{
 		return (0);
 	}
 	for (n = 0; b[n] != '\0'; n++)
 	{
-		j <<= 1;
-		if (b[n] == '1')
+		if (b[n] != '\0' && b[n] != '1')
 		{
-			j += 1;
+			return (0);
 		}
+		j = (j << 1) + (b[n] - '0');
 	}
 	return (j);
 
