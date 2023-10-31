c97eb9a0f5b30e11c12b7315f7842743fe69abb2
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-10-30 18:07:56 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -34,7 +34,7 @@ int create_file(const char *filename, char *text_content)
 	i = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	j = write(i, text_content, n);
-	
+
 	if (i == -1 || j == -1)
 	{
 		return (-1);
