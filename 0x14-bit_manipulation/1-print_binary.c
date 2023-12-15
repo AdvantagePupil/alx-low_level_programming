8831ddbcaf410622c114afa840375d31b33553e0
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-12-14 17:44:43 -0800

    task no.2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,4 +7,33 @@
  *
  * @n: number
  *
- * Return: 
+ * Return: always 0 (success)
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i;
+
+	int j;
+
+	i = sizeof(n) * 8;
+
+	j = 0;
+
+	while (i)
+	{
+		if (n & 1L << --i)
+		{
+			_putchar('1');
+			j++;
+		}
+		else if (j)
+		{
+			_putchar('0');
+		}
+	}
+	if (!j)
+	{
+		_putchar('0');
+	}
+}
