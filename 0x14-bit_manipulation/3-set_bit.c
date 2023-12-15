8f652e24f630312e47e6f3ed79e9096b530ca9f8
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-12-14 18:01:35 -0800

    task no.3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * set_bit - Entry point
+ *
+ * description: function that sets the value of a bit to 1 at a given index
+ *
+ * @n: number
+ *
+ * @index: index
+ *
+ * Return: value of a bit to 1 at a given index
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > (sizeof(unsigned long int) * 8 -1))
+	{
+		return (-1);
+	}
+	i = 1 << index;
+	*n = *n | i;
+	return (1);
+}
