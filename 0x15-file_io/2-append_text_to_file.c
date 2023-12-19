a50731024ae68b2d0ef2e3896def9dddb7c5314f
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-12-18 17:46:29 -0800

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,9 +27,9 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len]; len++)
+		for (len = 0; text_content[len];)
 		{
-			;
+			len++;
 		}
 	}
 
