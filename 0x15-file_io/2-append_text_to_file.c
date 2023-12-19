748175670c6b7ec5af3970e263800c96f9cab94a
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-12-18 17:45:30 -0800

    Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Entry point
+ *
+ * description: function that appends text at the end of a file
+ *
+ * @filename: filename
+ *
+ * @text_content: text content
+ *
+ * Return: Return: 1 on success and -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i = 0;
+
+	int j = 0;
+
+	int len = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; len++)
+		{
+			;
+		}
+	}
+
+	i = open(filename, O_RDONLY, O_APPEND);
+
+	j = write(i, text_content, len);
+
+	if (i == -1 || j == -1)
+	{
+		return (-1);
+	}
+
+	close(i);
+
+	return (1);
+}
