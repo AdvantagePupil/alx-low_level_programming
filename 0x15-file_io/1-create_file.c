08a4d72c72322c426b42ed2a4d71737e78f02f97
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-12-18 17:36:00 -0800

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	i = open(filename, O_CREAT | O_RDWR | O_TRUNC , 0600);
+	i = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	j = write(i, text_content, len);
 
