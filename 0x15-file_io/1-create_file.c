eccbeaaf0897f79744657f1c1e72126f478f8be8
AdvantagePupil <ahmadghaleb17@gmail.com>
2023-12-18 17:35:08 -0800

    Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * create_file - Entry point
+ *
+ * description: Create a function that creates a file.
+ *
+ * @filename: filename
+ *
+ * @text_content: the text content
+ *
+ * Return: Returns: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i = 0;
+
+	int j = 0;
+
+	int len = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	i = open(filename, O_CREAT | O_RDWR | O_TRUNC , 0600);
+
+	j = write(i, text_content, len);
+
+	if (i == -1 || j == -1)
+	{
+		return (-1);
+	}
+
+	close(i);
+
+	return (1);
+}
