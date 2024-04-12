46cea44341bc530d9f9a796306d6f3fdaa1714d5
AdvantagePupil <ahmadghaleb17@gmail.com>
2024-04-12 15:17:21 -0700

    hash tables

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Entry point.
+ *
+ * description: key index of the current item
+ *
+ * @key: The key to get the index of.
+ *
+ * @size: The size of the array item of the hash table.
+ *
+ * Return: The index of the key.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
